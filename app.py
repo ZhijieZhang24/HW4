8fb701fedd3ecc0d69c339bfdb3fb41370b568b7
ZhijieZhang24 <54722530+ZhijieZhang24@users.noreply.github.com>
2019-12-18 12:30:38 -0600

    search page added

--- app.py
+++ app.py
@@ -32,6 +32,18 @@ def index():
     all_singers = zzhijie_singersapp.query.all()
     return render_template('index.html', singers=all_singers, pageTitle='Zhijie Singers')
 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        form = request.form
+        search_value = form['search_string']
+        search = "%{0}%".format(search_value)
+        results = colbert_friends.query.filter(or_(colbert_friends.first_name.like(search),
+                                                    colbert_friends.last_name.like(search))).all()
+        return render_template('index.html', singers=results, pageTitle='Zhijie singers', legend="Search Results")
+    else:
+        return redirect('/')
+
 @app.route('/add_singer', methods=['GET', 'POST'])
 def add_singer():
     form = SingersForm()
@@ -54,6 +66,28 @@ def delete_singer(singerid):
 
     else: #if it's a GET request, send them to the home page
         return redirect("/")
+
+@app.route('/singer/<int:singer_id>', methods=['GET','POST'])
+def get_singer(singer_id):
+    singer = zzhijie_singersapp.query.get_or_404(singer_id)
+    return render_template('singer.html', form=singer, pageTitle='Singer Details', legend="Singer Details")
+
+@app.route('/singer/<int:singer_id>/update', methods=['GET','POST'])
+def update_singer(singer_id):
+    singer = zzhijie_singersapp.query.get_or_404(singer_id)
+    form = SingersForm()
+
+    if form.validate_on_submit():
+        singer.first_name = form.first_name.data
+        singer.last_name = form.last_name.data
+        db.session.commit()
+        return redirect(url_for('get_singer', singer_id=singer.singerid))
+
+    form.singerid.data = singer.singerid
+    form.first_name.data = friend.first_name
+    form.last_name.data = friend.last_name
+    return render_template('update_singer.html', form=form, pageTitle='Update Singer', legend="Update A Singer")
+
 
 
 
